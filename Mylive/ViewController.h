1a8c8353219238d626ef9517faa501f292e4e796
XCwuhao <2248975397@qq.com>
2016-04-12 22:32:04 +0800

    上传项目

---
+++ Mylive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mylive
+//
+//  Created by apple on 16/4/12.
+//  Copyright © 2016年 XC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Mylive/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Mylive
+//
+//  Created by apple on 16/4/12.
+//  Copyright © 2016年 XC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"HelloWord");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
